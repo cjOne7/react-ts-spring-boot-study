1a3a39bb58a3d0194a4ed36f65729f4d573ad5d2
cjOne7 <nikyarosh07@gmail.com>
2022-08-22 10:16:04 +0200

    added init project

---
+++ todo/src/test/java/eu/profinit/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.profinit.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
